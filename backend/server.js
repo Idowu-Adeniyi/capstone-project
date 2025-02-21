34f3cf9572ccc2399b4ef1b890c2d26cb20492bf
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-02-21 01:49:48 -0500

    updated backend server

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
-const app = express();
-
-app.use(express.json());
-app.use(cors());
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const workLogRoutes = require("./routes/workLogRoutes");
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/worklogs", workLogRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(5000, () => console.log("Server running on port 5000"));
-  })
-  .catch((err) => console.error(err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
