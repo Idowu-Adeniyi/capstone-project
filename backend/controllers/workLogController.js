db9b8295804d0b4e6bd65a5e581b9c976af48861
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-02-21 02:21:12 -0500

    updated backend worklog

--- backend/controllers/workLogController.js
+++ backend/controllers/workLogController.js
@@ -1,21 +1,39 @@
-const WorkLog = require("../models/WorkLog");
+import WorkLog from "../models/WorkLog.js";
 
-exports.createWorkLog = async (req, res) => {
-  try {
-    const workLog = new WorkLog({ ...req.body, user: req.user.id });
-    await workLog.save();
-    res.status(201).json(workLog);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+export const createWorkLog = async (req, res) => {
+  const { date, hours, description } = req.body;
+  const workLog = await WorkLog.create({
+    user: req.user._id,
+    date,
+    hours,
+    description,
+  });
+  res.status(201).json(workLog);
 };
 
-exports.getUserWorkLogs = async (req, res) => {
-  const logs = await WorkLog.find({ user: req.user.id });
+export const getUserWorkLogs = async (req, res) => {
+  const logs = await WorkLog.find({ user: req.user._id }).sort("-date");
   res.json(logs);
 };
 
-exports.getAllWorkLogs = async (req, res) => {
-  const logs = await WorkLog.find().populate("user", "name email");
+export const getAllWorkLogs = async (req, res) => {
+  const logs = await WorkLog.find().populate("user", "name").sort("-date");
   res.json(logs);
+};
+
+export const getWorkHoursReport = async (req, res) => {
+  const report = await WorkLog.aggregate([
+    { $group: { _id: "$user", totalHours: { $sum: "$hours" } } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "_id",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    { $unwind: "$user" },
+    { $project: { user: "$user.name", totalHours: 1 } },
+  ]);
+  res.json(report);
 };
