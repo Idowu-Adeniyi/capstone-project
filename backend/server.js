7c2b5cefc263abd0acae58890e281194443ab31a
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-02-21 02:14:20 -0500

    backend server updated

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import workLogRoutes from "./routes/workLogRoutes.js";
 
 dotenv.config();
-
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-const workLogRoutes = require("./routes/workLogRoutes");
-
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected."))
+  .catch((err) => console.error(err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/worklogs", workLogRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
